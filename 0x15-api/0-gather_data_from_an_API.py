f41d017fb92cce51abf74eb5fd00c2a69e68c7c1
chapaedb <chapaeresso1@gmail.com>
2024-03-25 13:32:06 -0700

    update

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -4,32 +4,24 @@ Script that retrieves information about a user's TODO list progress
 from a REST API using their employee ID.
 """
 
-import json
 import requests
 from sys import argv
 
-
 if __name__ == "__main__":
-    session = requests.Session()
-
     employee_id = argv[1]
-    todos_url = 'https://jsonplaceholder.typicode.com/users/{}/todos'.format(employee_id)
-    user_url = 'https://jsonplaceholder.typicode.com/users/{}'.format(employee_id)
+    todos_url = f"https://jsonplaceholder.typicode.com/users/{employee_id}/todos"
+    user_url = f"https://jsonplaceholder.typicode.com/users/{employee_id}"
 
-    employee_response = session.get(todos_url)
-    user_response = session.get(user_url)
+    employee_response = requests.get(todos_url)
+    user_response = requests.get(user_url)
 
     todos_data = employee_response.json()
-    user_name = user_response.json()[0]['name']
+    user_name = user_response.json()['name']
 
-    total_tasks = 0
+    total_tasks = len(todos_data)
+    completed_tasks = [task for task in todos_data if task['completed']]
 
-    for task in todos_data:
-        if task['completed']:
-            total_tasks += 1
+    print(f"Employee {user_name} is done with tasks({len(completed_tasks)}/{total_tasks}):")
 
-    print("Employee {} has completed tasks: {}/{}".format(user_name, total_tasks, len(todos_data)))
-
-    for task in todos_data:
-        if task['completed']:
-            print("\t" + task['title'])
+    for task in completed_tasks:
+        print(f"\t{task['title']}")
